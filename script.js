09bcdaaf6c762dff0ad0c2e9531a2095368f0efd
Callum Gilfedder <Callumgilf@mail.com>
2022-11-23 13:53:55 +0000

    Added grid checker

--- script.js
+++ script.js
@@ -81,6 +81,80 @@ function check() {
             } 
         } 
     }
+    var grid1 = ["1","2", "3", "10", "11", "12", "19", "20", "21"]
+    var grid2 = ["4", "5", "6", "13", "14", "15", "22", "23", "24"]
+    var grid3 = ["7", "8", "9", "16", "17", "18", "25", "26", "27"]
+    var grid4 = ["28", "29", "30", "37", "38", "39", "46", "47", "48"]
+    var grid5 = ["31", "32", "33", "40", "41", "42", "49", "50", "51"]
+    var grid6 = ["34", "35", "36", "43", "44", "45", "52", "53", "54"]
+    var grid7 = ["55", "56", "57", "64", "65", "66", "73", "74", "75"]
+    var grid8 = ["58", "59", "60", "67", "68", "69", "76", "77", "78"]
+    var grid9 = ["61", "62", "63", "70", "71", "72", "79", "80", "81"]
+    var inGrid= "";
+
+    if (grid1.includes(savedSquare.id)) {
+        inGrid = grid1; 
+        console.log(inGrid)
+    } else if (grid2.includes(savedSquare.id)) {
+        inGrid = grid2;
+        console.log(inGrid);
+        
+    } else if (grid3.includes(savedSquare.id)) {
+        inGrid = grid3;
+        console.log(inGrid);
+    } else if (grid4.includes(savedSquare.id)) {
+        inGrid = grid4;
+        console.log(inGrid);
+    } else if (grid5.includes(savedSquare.id)) {
+        inGrid = grid5;
+        console.log(inGrid);
+    } else if (grid6.includes(savedSquare.id)) {
+        inGrid = grid6;
+        console.log(inGrid);
+    } else if (grid7.includes(savedSquare.id)) {
+        inGrid = grid7;
+        console.log(inGrid);
+    } else if (grid8.includes(savedSquare.id)) {
+        inGrid = grid8;
+        console.log(inGrid);
+    } else if (grid9.includes(savedSquare.id)) {
+        inGrid = grid9;
+        console.log(inGrid);
+    }
+
+    for (elements of inGrid) {
+        // console.log(elements)
+        console.log(grid[parseInt(elements)])
+        if (savedSquare.id != elements-1) {
+            if (savedSquare.innerHTML == grid[parseInt(elements-1)].innerHTML) {
+                // console.log("Elements" + grid[parseInt(elements)].innerHTML)
+                // console.log("Saved square: "+ savedSquare.innerHTML)
+                // var j = 0;
+                // while (j < grid.length) {
+                //     // 
+                //     if (grid[j].style.backgroundColor == "rgb(255, 160, 122)" || grid[j].style.color == "rgb(255, 0, 0)")  {
+                //         if (grid[j].className == "square") {
+                //             grid[j].style.backgroundColor = "white";
+                //             grid[j].style.color = "black";
+                //         } else {
+                //             grid[j].style.backgroundColor = "lightgrey";
+                //             grid[j].style.color = "black";
+                //         }
+                //     }
+                //     j++; 
+                // }
+                savedSquare.style.color = "red";
+                if (grid[parseInt(elements-1)].id != savedSquare.id) {
+                grid[parseInt(elements-1)].style.backgroundColor = "#FFA07A"; 
+                }
+            }
+        }
+    }
+    
+    
+    
+
+
 
 
 
